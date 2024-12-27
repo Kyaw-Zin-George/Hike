824a7629b6f58322873e8664596357603bb3a2f2
Kyaw-Zin-George <ktz1695@gmail.com>
2024-12-27 09:08:35 +0000

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Kyaw Thant Zin(George) on 12/27/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
